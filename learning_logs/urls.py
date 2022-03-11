f38a0797a032d2960b90d759c50ab50e259c73a5
fis0i <osya.pitersky@gmail.com>
2022-03-11 18:05:26 +0300

    learning log 1

---
+++ learning_logs/urls.py
@@ -0,0 +1,13 @@
+# Определяет схемы URL для learning_logs.
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic')
+]
